025489d3193100c67a86a445ca6be8cc0b0034cb
LevPewPew <levtoth1988@gmail.com>
2020-01-14 19:31:50 +1100

    changed boilerplate info to be relvent to makeup-by-ellie

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ const index = (req, res) => {
 
 router.get('/', index);
 
-// define routes here, this will mean anything in the sushi-routes.js file (yet to be created) will assume that /sushi will prepend the route path defined in sushi-routes.js
 router.use('/categories', require('./categories-routes'));
 
 module.exports = router;
