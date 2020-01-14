84fb4fbe215f5ebaaec9eb74eae009c7672b8cfa
LevPewPew <levtoth1988@gmail.com>
2020-01-14 15:33:31 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
+const dbConfig = { useNewUrlParser: true, useUnifiedTopology: true };
+const whitelist = ['http://localhost:3000', 'https://sushi-dev-portal.netlify.com'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+mongoose.connect(DB_URL, dbConfig, (err) => {
+  if(err) {
+    console.log("MongooseDB Connection Error");
+  } else {
+    console.log("Connected to MongooseDB");
+  }
+});
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({corsOptions}));
+app.use(require('./routes/index'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
