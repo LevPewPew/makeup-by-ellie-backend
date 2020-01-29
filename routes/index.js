b52a9de4738e2d14c649a3cdcd455ce9e993109c
LevPewPew <levthedev@protonmail.com>
2020-01-29 16:49:19 +1100

    as branch name

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.use('/services', require('./service-routes'));
 
 router.use('/contact', require('./contact-routes'));
 
-router.use('/FAQ', require('./question-routes'));
+router.use('/faq', require('./question-routes'));
 
 router.use('/aws-s3', require('./aws-s3-routes'));
 
