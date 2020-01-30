d0400a58f4d2ad014750ef07fe7ff519b9a26a19
LevPewPew <levthedev@protonmail.com>
2020-01-30 12:26:33 +1100

    created editing route for questions

--- routes/question-routes.js
+++ routes/question-routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { index, create, remove } = require('../controllers/question-controller');
+const { index, create, edit, remove } = require('../controllers/question-controller');
 
 router.get('/', index);
 
 // for Admin
 router.post('/', create);
+router.put('/:id', edit);
 router.delete('/:id', remove);
 
 module.exports = router;
